af1d981dbc5fb7d34831c538f002795da33dd038
panwaslucibinong <142313979+panwaslucibinong@users.noreply.github.com>
2023-11-06 13:48:19 +0700

    Update db.js

--- utils/db.js
+++ utils/db.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
-// mongoose.connect('mongodb+srv://panwas:6D4dRPYrHWTZA5gH@cluster0.syun2c8.mongodb.net/Panwas', {
-mongoose.connect('mongodb://127.0.0.1:27017/Panwas', {
+mongoose.connect('mongodb+srv://panwas:6D4dRPYrHWTZA5gH@cluster0.syun2c8.mongodb.net/Panwas', {
+// mongoose.connect('mongodb://127.0.0.1:27017/Panwas', {
     useNewUrlParser: true,
     useUnifiedTopology: true
 })
